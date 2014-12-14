106fe0fd722615f67f75978716c8827d2fc247ca
Brendan <brendan@BrenCompRobo.(none)>
2014-12-14 16:20:23 -0500

    Finds Ts, reports exit angles

--- comprobofinalproject/scripts/intersectionFinder.py
+++ comprobofinalproject/scripts/intersectionFinder.py
@@ -9,7 +9,7 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from comprobofinalproject.msg import Intersection
-
+import copy
 class LineFinder:
 	def __init__(self):
 		rospy.init_node('line_finder', anonymous = True)
@@ -20,7 +20,7 @@ class LineFinder:
 		self.image = None
 		self.odometry = None
 
-		self.polynomial = [-5.66680774e-07,5.68132807e-04,-2.17597871e-01,5.62396178e+01]
+		self.polynomial = [3.28735261e-09,-4.32672402e-06,2.17941676e-03,-5.29013494e-01,7.86939929e+01]
 		self.ignoredBorder = [200,25,50,50]
 		self.found = 0
 		self.exitBorder = [150,50,50,50]
@@ -59,26 +59,28 @@ class LineFinder:
 		cv2.setTrackbarPos('thresh',self.name,9)
 
 
-	def intersectionCenter(self,corners):
-		contours, heiarchy = cv2.findContours(corners,1,2)
+	def intersectionCenter(self,corner_mask):
+		contours, heiarchy = cv2.findContours(corner_mask,1,2)
 		if len(contours) > 0:
 			totalx = 0.0
 			totaly = 0.0
 			total = 0
+			corners = []
 			for contour in contours:
-				if cv2.contourArea(contour) > 100:
+				if cv2.contourArea(contour) > 300:
 					M = cv2.moments(contour)
 					if M['m00'] == 0:
 						print "WARNING: m00 == 0"
 					else:
 						cx = int(M['m10']/M['m00'])
 						cy = int(M['m01']/M['m00'])
+						corners.append((cx,cy))
 						totalx += cx
 						totaly += cy
 						total += 1
 			if(total>=2):
-				return (int(round(totalx/total)),int(round(totaly/total)))
-		return None
+				return ((int(round(totalx/total)),int(round(totaly/total))),corners)
+		return (None,None)
 
 	def exitPathCenters(self, border):
 		contours, heiarchy = cv2.findContours(border,1,2)
@@ -95,11 +97,24 @@ class LineFinder:
 						exits.append((cx,cy))
 			return exits
 		return []
-		
+
+	def exitAngle(self,exit,intersection):
+		return math.atan2(intersection[0]-exit[0],float(intersection[1]-exit[1])) 
+
+	def intersection(self,a,b,c,d):
+		return (((a[0]*b[1]-a[1]*b[0])*(c[0]-d[0])-(a[0]-b[0])*(c[0]*d[1]-c[1]*d[0]))/((a[0]-b[0])*(c[1]-d[1])-(a[1]-b[1])*(c[0]-d[0])),
+			((a[0]*b[1]-a[1]*b[0])*(c[1]-d[1])-(a[1]-b[1])*(c[0]*d[1]-c[1]*d[0]))/((a[0]-b[0])*(c[1]-d[1])-(a[1]-b[1])*(c[0]-d[0])))
+
+	def slope(self,a,b):
+		return (math.atan2(a[1]-b[1],float(a[0]-b[0])) + math.pi) % math.pi
+
+
+
 	def run(self):
 		r=rospy.Rate(20)
 		while not rospy.is_shutdown():
 			if self.image != None:
+				odom = copy.deepcopy(self.odometry)
 				frame = self.image
 				
 				#RED MASKING TAPE HSV
@@ -128,48 +143,58 @@ class LineFinder:
 				shifted_dst[self.ignoredBorder[0]:-self.ignoredBorder[1], self.ignoredBorder[2]:-self.ignoredBorder[3]] = dst
 				corner_pic = frame.copy()
 
-
 				corner_mask = np.zeros((len(borderless),len(borderless[0])),np.uint8)
 				power = cv2.getTrackbarPos('thresh',self.name)
 				dst_threshhold = 10**(-power)
 				corner_pic[shifted_dst>dst_threshhold] = [255,0,0]
 				corner_mask[dst>dst_threshhold] = 1
-				print .01*dst.max()
 
+				(intersectionPoint,centers) = self.intersectionCenter(corner_mask)
 
-				intersectionPoint = self.intersectionCenter(corner_mask)
 				if(intersectionPoint != None):
 					self.found += 1
 					if(self.found > 4):
 						intersectionPoint = (intersectionPoint[0]+self.ignoredBorder[2],intersectionPoint[1]+self.ignoredBorder[0])
-						cv2.circle(corner_pic,intersectionPoint,2,(0,255,0),2)
-
-						p = np.poly1d(np.array(self.polynomial))
-						dist = p(intersectionPoint[0])
-
-						odom = self.odometry
-						x = odom.pose.pose.position.x
-						y = odom.pose.pose.position.y
-						z = math.atan2(2* (odom.pose.pose.orientation.z * odom.pose.pose.orientation.w),1 - 2 * ((odom.pose.pose.orientation.y)**2 + (odom.pose.pose.orientation.z)**2))
-						odom.pose.pose.position.x = x + dist * math.cos(z)
-						odom.pose.pose.position.y = y + dist * math.sin(z)
+						cv2.circle(corner_pic,intersectionPoint,2,(255,0,0),2)
 
 
 						#Crop mask
 						border = mask
 						border[self.exitBorder[0]:-self.exitBorder[1],self.exitBorder[2]:-self.exitBorder[3]] = 0
-						
-
-						
-
 						exits = self.exitPathCenters(mask)
 
+						if(len(exits) == 3 and len(centers) == 2):
+							center_slope = self.slope(centers[0],centers[1])
+							perp_slope = (center_slope + (math.pi/2)) % math.pi
+							#print "slope", center_slope
+							#print "perp", perp_slope
+							slopes = []
+							exits_copy = copy.deepcopy(exits)
+							for each in exits_copy:
+								#print self.slope(intersectionPoint,each)
+								slopes.append(self.slope(intersectionPoint,each))
+							index = slopes.index(min(slopes,key = lambda x: min(abs(x - perp_slope),math.pi - abs(x-perp_slope))))
+							perp_point = exits_copy.pop(index)
+							intersectionPoint = self.intersection(perp_point,intersectionPoint,exits_copy[0],exits_copy[1])
+							intersectionPoint = (int(round(intersectionPoint[0])),int(round(intersectionPoint[1])))
+							cv2.circle(corner_pic,intersectionPoint,2,(0,255,0),2)
+
+						p = np.poly1d(np.array(self.polynomial))
+						dist = p(intersectionPoint[1])
+
+						x = odom.pose.pose.position.x
+						y = odom.pose.pose.position.y
+						z = math.atan2(2* (odom.pose.pose.orientation.z * odom.pose.pose.orientation.w),1 - 2 * ((odom.pose.pose.orientation.y)**2 + (odom.pose.pose.orientation.z)**2))
+						x = x + dist * math.cos(z)
+						y = y + dist * math.sin(z)
+
+						angles = []
 						for each in exits:
+							angles.append((z + self.exitAngle(each,intersectionPoint) + math.pi) % (2 * math.pi) - math.pi)
+							#angles.append(self.exitAngle(each,intersectionPoint))
 							cv2.line(corner_pic,intersectionPoint,each,(0,255,0),2)
-
-
-						inter = Intersection(distance = dist,odom = odom)
-						#print "Found Intersections:", inter
+						
+						inter = Intersection(x = x, y = y, z = z, exits = angles,distance = dist,odom = odom)
 
 						self.dist_pub.publish(inter)
 				else:
