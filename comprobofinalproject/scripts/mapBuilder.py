69895f917310c90e0872b9fc68df99f0d23599fa
Mac-I Crowell <macicrowell@gmail.com>
2014-12-18 06:38:27 -0500

    it drives well and makes a map

--- comprobofinalproject/scripts/mapBuilder.py
+++ comprobofinalproject/scripts/mapBuilder.py
@@ -8,33 +8,69 @@ import cv2
 import uuid
 
 import numpy as np
+import pylab as pl
 import math
 
-import random
+from matplotlib import collections  as mc
 
+import random
+from std_msgs.msg import String
+import matplotlib.pyplot as plt
 from comprobofinalproject.srv import *
 
-global nodeGraph = {}
+global nodeGraph
 
 class DecisionMaker:
     def __init__(self):
         rospy.init_node('getTurn')
+        self.task = "Random"
         rospy.Subscriber("task", String, self.taskChange)
+        rospy.Subscriber("mapVisual", String, self.mapVisual)
         s = rospy.Service('getTurn', intersectionFoundGetTurn, self.handleNewIntersection)
         self.mapBuilder = MapBuilder()
         print "Ready to pick Direction."
 
     def taskChange(self,msg):
-        self.task = msg
+        self.task = msg.data
+
+    def mapVisual(self,msg):
+        global nodeGraph
+        self.visualMap = VisualMap(nodeGraph)
 
     def handleNewIntersection(self,msg):
         if self.task == "Map":
-            self.mapBuilder.incomingIntersection(msg)
+            direction = self.mapBuilder.incomingIntersection(msg)
+            return intersectionFoundGetTurnResponse(exit_chosen = direction)
         elif self.task == "Path":
             pass
         else:
             ranInt = random.randrange(0,len(msg.exits))
-            return intersectionFoundGetTurnResponse(exit_chosen = msg.exits[ranInt],exit_chosen_raw = msg.exits_raw[ranInt])
+            return intersectionFoundGetTurnResponse(exit_chosen = msg.exits[ranInt])
+
+class VisualMap:
+    def __init__(self,nodeGraph):
+        self.nodeGraph = nodeGraph
+        self.plotGraph()
+
+    def plotGraph(self):
+        xPoints = []
+        yPoints = []
+        lines = []
+
+        for uuid, knownIntersection in self.nodeGraph.iteritems():
+            xPoints.append(knownIntersection.x)
+            yPoints.append(knownIntersection.y)
+            for exit in knownIntersection.getVisitedExits():
+                lines.append([(knownIntersection.x,knownIntersection.y),(nodeGraph[exit.connectedNodeId].x,nodeGraph[exit.connectedNodeId].y)])
+
+        lc = mc.LineCollection(lines, linewidths=2)
+        fig, ax = pl.subplots()
+        pl.plot(xPoints, yPoints, 'ro')
+        ax.add_collection(lc)
+        ax.autoscale()
+        ax.margins(0.1)
+        pl.show()
+
 
 class MapBuilder:
     def __init__(self):
@@ -42,6 +78,7 @@ class MapBuilder:
         nodeGraph = {}
         self.currentIntersectionUUID = None
         self.previousDirection = None
+        self.currentDirection = None
 
     def incomingIntersection(self,msg):
         global nodeGraph
@@ -52,41 +89,54 @@ class MapBuilder:
         newIntersection = self.createDetailedIntersectionFromMsg(msg)
 
         if len(nodeGraph) == 0:
+            print "added node to empty graph"
             nodeGraph[newIntersection.uuid] = newIntersection
             self.currentIntersectionUUID = newIntersection.uuid
         else:
-            known = isKnownIntersection(newIntersection)
+            known = self.isKnownIntersection(newIntersection)
             if known == False:
-                addNewIntersectionToMap(self.previousIntersectionUUID, self.previousDirection, newIntersection,msg)
+                print "adding new node to graph"
+                self.currentIntersectionUUID = newIntersection.uuid
+                self.addNewIntersectionToMap(self.previousIntersectionUUID, self.previousDirection, newIntersection,msg)
             else:
+                print "updating location to existing node"
                 self.currentIntersectionUUID = known
-                updateMap(self.previousIntersectionUUID, self.previousDirection, self.currentIntersectionUUID)
+                self.updateMap(self.previousIntersectionUUID, self.previousDirection, self.currentIntersectionUUID,msg)
 
         currentIntersection = nodeGraph[self.currentIntersectionUUID]
-        if len(currentIntersection.getNotVisitedExits()) == 0:
-            
+        notVisitedExits = currentIntersection.getNotVisitedExits()
+        if len(notVisitedExits) == 0:
+            print "All Exits Visited"
+            ranInt = random.randrange(0,len(currentIntersection.exits))
+            direction = currentIntersection.exits[ranInt].angle
+            self.currentDirection = (direction,ranInt)
         else:
+            print "Going To Unvisited Exit"
+            chosenExit = random.choice(notVisitedExits)
+            chosenIndex = currentIntersection.exits.index(chosenExit)
+            self.currentDirection = (chosenExit.angle,chosenIndex)
 
-
-        self.currentDirection = (value,index)
+        print "Sending direction: " + str(self.currentDirection)
+        return self.currentDirection[0]
 
     def addNewIntersectionToMap(self,previousUUID,previousDirection,newIntersection,msg):
         global nodeGraph
         nodeGraph[newIntersection.uuid] = newIntersection
         exits = nodeGraph[previousUUID].exits
         nodeGraph[previousUUID].exits[previousDirection[1]].connectedNodeId = newIntersection.uuid
-        nodeGraph[newIntersection.uuid].exits[len(exits-1)].connectedNodeId = previousUUID
+        nodeGraph[newIntersection.uuid].exits[-1].connectedNodeId = previousUUID
 
     def updateMap(self,previousUUID,previousDirection,currentUUID,msg):
         global nodeGraph
         nodeGraph[previousUUID].exits[previousDirection[1]].connectedNodeId = currentUUID        
         exits = nodeGraph[currentUUID].exits
-        closestExitIndex = min(range(len(exits)), key=lambda i: abs(math.atan2(math.sin(exits[i]-msg.current_path_exit), math.cos(exits[i]-msg.current_path_exit))))
-        nodeGraph[newIntersection.uuid].exits[len(exits-1)].connectedNodeId = previousUUID
+        closestExitIndex = min(range(len(exits)), key=lambda i: abs(math.atan2(math.sin(exits[i].angle-msg.current_path_exit), math.cos(exits[i].angle-msg.current_path_exit))))
+        nodeGraph[currentUUID].exits[closestExitIndex].connectedNodeId = previousUUID
 
     def isKnownIntersection(self,intersection):
-        for uuid, knownIntersection in d.iteritems():
-            dist = euclidDistance(intersection.x,intersection.y,knownIntersection.x,knownIntersection.y)
+        global nodeGraph
+        for uuid, knownIntersection in nodeGraph.iteritems():
+            dist = self.euclidDistance(intersection.x,intersection.y,knownIntersection.x,knownIntersection.y)
             if dist < .10:
                 return uuid
         return False
@@ -113,8 +163,15 @@ class DetailedIntersection:
 
     def getNotVisitedExits(self):
         exitList = []
-        for exit in exits:
-            if not exit.isVisited:
+        for exit in self.exits:
+            if not exit.isVisited():
+                exitList.append(exit)
+        return exitList
+
+    def getVisitedExits(self):
+        exitList = []
+        for exit in self.exits:
+            if exit.isVisited():
                 exitList.append(exit)
         return exitList
 
@@ -124,7 +181,7 @@ class Exit:
         self.connectedNodeId = connectedNodeId
 
     def isVisited(self):
-        if self.connectedNodeID == None:
+        if self.connectedNodeId == None:
             return False
         else:
             return True
@@ -135,7 +192,7 @@ def main(args):
     ic = DecisionMaker()
 
     #set ROS refresh rate
-    r = rospy.Rate(90)
+    r = rospy.Rate(30)
 
     #keep program running until shutdown
     while not(rospy.is_shutdown()):
